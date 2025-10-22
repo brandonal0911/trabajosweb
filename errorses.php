55774eab3c2b8b9b1386f425b431d1fd6e399fb6
brandonal0911 <brandonhernandezn82@gmail.com>
2025-10-22 17:42:45 -0600

    Add files via upload

---
+++ errorses.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+</head>
+<body>
+	<h1>Error al iniciar sesión</h1>
+	<p>Usuario o contraseña incorrectos. Por favor verifique las credenciales</p>
+	<br>
+	<form method="GET" action="login.html">
+		<input name="Regresar" value="Regresar" type="submit">
+	</form>
+</body>
+</html>
